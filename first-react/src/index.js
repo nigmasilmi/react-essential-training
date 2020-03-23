3778c765c9b119f8af65965c4b09683c8dd457df
nigmasilmi <nigmasilmi.web@gmail.com>
2020-03-22 21:21:07 -0300

    First Class type component with customized methods

---
+++ first-react/src/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Data
+const studyHoursData = {
+
+  average: 6,
+  daysPerWeek: 7,
+  codingPerSession: 4,
+  dummy: "yeah"
+
+};
+
+// Component
+
+class MeasureMyNerdness extends React.Component {
+
+  codingPerWeek(average, codingPerSession) {
+    return average * codingPerSession;
+  }
+
+
+  render() {
+    const { average, daysPerWeek, codingPerSession } = this.props;
+    return (
+      <section className="sectionWrapper">
+        <div><p>
+          My Average of study days in a week is {average}
+        </p></div>
+        <div><p>
+          I am used to study {daysPerWeek} days per week <br />
+        </p></div>
+        <div><p>
+          It includes theory and {codingPerSession} coding hours per session
+        </p></div>
+        <div><p>
+          It means that my coding hours in a week are: {this.codingPerWeek(average, codingPerSession)} plus, hours
+        </p></div>
+      </section>
+    )
+  }
+
+}
+
+
+
+ReactDOM.render(
+  // Component to render with specific props values if there are any
+  <MeasureMyNerdness
+    average={studyHoursData.average}
+    daysPerWeek={studyHoursData.daysPerWeek}
+    codingPerSession={studyHoursData.codingPerSession}
+
+  />,
+  document.getElementById("root")
+);
