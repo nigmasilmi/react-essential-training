f57d437e0022c6cfb712740b427b4bc9a656ecf9
nigmasilmi <nigmasilmi.web@gmail.com>
2020-03-24 20:17:22 -0300

    Display Child components and mapping data

--- first-react/src/index.js
+++ first-react/src/index.js
@@ -1,30 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+let equipment = [
+  {
+    brand: "Brooks",
+    model: "Ghost 9",
+    type: "Cushioning",
+    sole: "Neutral",
+    kms: 30
+  },
+  {
+    brand: "Asics",
+    model: "Gel Superion",
+    type: "Stability",
+    sole: "Pronation",
+    kms: 35
+  },
+  {
+    brand: "New Balance",
+    model: "1080 V4",
+    type: "Cushioning",
+    sole: "Neutral",
+    kms: 20
+  },
+  {
+    brand: "Saucony",
+    model: "Peregrine 8",
+    type: "Trail",
+    sole: "Neutral",
+    kms: 50
+  },
+];
 
-const KmsCounter = ({ equipOne, equipTwo }) => {
+// calc function
+
+const addUpThoseKms = (equipment) => {
+  let total = 0;
+  for (let pair of equipment) {
+
+    total += pair.kms;
+
+  }
+  return total;
+};
+
+const KmsCounter = () => {
   return (
     <section>
       <article>
-        <div> <strong>I am the Child component</strong> and this is the Weekley Millage so far: {equipOne} + {equipTwo} </div>
+        <div> <strong>I am the Child component</strong> and this is the Weekley Millage so far: {addUpThoseKms(equipment)}  </div>
       </article>
     </section>
   )
 };
 
-const EquipShare = ({ asicsKms, brooksKms }) => {
+
+
+const Pair = ({ brand, model, kms }) => {
+  return (
+    <div>
+      <h3>
+        The {brand} / {model} are onboard with: {kms} kms
+      </h3>
+    </div>
+  )
+};
+
+
+const EquipShare = ({ equipment }) => {
   return (
     <section>
-      <div>The Asics are onboard with {asicsKms} Kms </div>
-      <div>The Brooks are onboard with {brooksKms} Kms </div>
-      <KmsCounter equipOne={asicsKms} equipTwo={brooksKms} />
-    </section>
+      <div>{equipment.map((pair, i) => <Pair key={i} brand={pair.brand} model={pair.model} kms={pair.kms} />)}</div>
+      <KmsCounter />
+    </section >
+
+
   )
 };
 
 ReactDOM.render(
   <div>
-    <EquipShare asicsKms={50} brooksKms={90} />
+    <EquipShare equipment={equipment} />
   </div>,
   document.getElementById("root")
 
